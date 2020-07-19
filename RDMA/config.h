2e8043d574545c034e2e190f840e983676930707
Benyamin <beneslami72@gmail.com>
2020-07-19 15:03:56 +0430

    configurations

---
+++ RDMA/config.h
@@ -0,0 +1,40 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <stdbool.h>
+#include <inttypes.h>
+
+enum ConfigFileAttr {
+    ATTR_SERVERS = 1,
+    ATTR_CLIENTS,
+    ATTR_MSG_SIZE,
+    ATTR_NUM_CONCURR_MSGS,
+};
+
+struct ConfigInfo {
+    int  num_servers;
+    int  num_clients;
+    char **servers;          /* list of servers */
+    char **clients;          /* list of clients */
+
+    bool is_server;          /* if the current node is server */
+    int  rank;               /* the rank of the node */
+
+    int  msg_size;           /* the size of each echo message */
+    int  num_concurr_msgs;   /* the number of messages can be sent concurrently */
+
+    char *sock_port;         /* socket port number */
+}__attribute__((aligned(64)));
+
+extern struct ConfigInfo config_info;
+
+int
+parse_config_file(char*);
+
+void
+destroy_config_info ();
+
+void
+print_config_info ();
+
+#endif
